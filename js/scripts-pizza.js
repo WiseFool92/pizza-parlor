1047970e0c1de0700f93b72c5a832bc17050bdc8
Nathan Watkins Hoagland <watkins92@gmail.com>
2020-04-10 11:37:20 -0700

    corrected html select box for here or to go, inital js frontend logic

--- js/scripts-pizza.js
+++ js/scripts-pizza.js
@@ -1,6 +1,18 @@
+// Backend Buisness logic
+
 function Parlor (size, toppings, quantity, forHereOrToGo) {
   this.size = size;
   this.toppings = toppings;
   this.quantity = quantity;
   this.forHereOrToGo = forHereOrToGo;
 }
+
+//frontend UI logic
+
+$(document).ready(function() {
+
+  $("form").submit(function(event) {
+  event.preventDefault();
+  
+  })
+})
